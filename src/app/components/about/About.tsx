dea91be12f054114b00f7232f3d5b3da945c982f
Yan Moe Naing <yan.webdev.1234@gmail.com>
2023-10-22 16:35:12 +0630

    framer motion added

--- src/app/components/about/About.tsx
+++ src/app/components/about/About.tsx
@@ -2,14 +2,35 @@
 import Container from "@/ui/layout/container/Container";
 import SectionTitle from "@/ui/shared/SectionTitle";
 import styles from "./About.module.css";
+import { useAnimate, useInView } from "framer-motion";
+import { useEffect } from "react";
 export default function About() {
- 
+  const [scope, animate] = useAnimate();
+  const isInView = useInView(scope, {
+    margin: "-400px",
+  });
+
+  useEffect(() => {
+    if (isInView) {
+      animate(`.${styles.aboutTitle}`, {
+        transform: "translateX(0)",
+        opacity: 1,
+      });
+    }
+  }, [isInView]);
+
   return (
-    <section className={styles.about}>
+    <section ref={scope} className={styles.about}>
       <Container>
-        <SectionTitle title="who we are" />
-        <p className="mt-3">
-        We build custom software applications and provide solution that will fit your business’s need. We are a team of skilled developers and designers to help build user centric applications for your business. We will listen your ideas and transform it into a product your customers will love and use in daily life. 
+        <div className={styles.aboutTitle}>
+          <SectionTitle title="who we are" />
+        </div>
+        <p className={`mt-3 ${styles.aboutTitle}`}>
+          We build custom software applications and provide solution that will
+          fit your business’s need. We are a team of skilled developers and
+          designers to help build user centric applications for your business.
+          We will listen your ideas and transform it into a product your
+          customers will love and use in daily life.
         </p>
       </Container>
     </section>
